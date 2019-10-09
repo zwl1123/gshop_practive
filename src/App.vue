fb21b147d6da6b3fba2079b9f6ef34beda623de4
zwl1123 <18826077872@163.com>
2019-10-09 13:39:58 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FootGuide  v-show="$route.meta.showFooter"></FootGuide>
+  </div>
+</template>
+
+<script>
+  import FootGuide from './components/FootGuide/FootGuide'
+  export default {
+    components:{
+      FootGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
+    position relative
+</style>
